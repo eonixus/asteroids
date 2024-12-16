36a75b9b482c440b4fde96fa5276c26583a87bda
eonixus <dvidcpo@gmail.com>
2024-12-17 00:12:18 +0700

    add exit button and fps

--- main.py
+++ main.py
@@ -6,12 +6,22 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
     print("Starting asteroids!")
 
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
         screen.fill("black")
         pygame.display.flip()
+        
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
