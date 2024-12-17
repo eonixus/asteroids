db14be9d202e31b1321113f0a0957a5a81b8f270
eonixus <dvidcpo@gmail.com>
2024-12-17 17:47:28 +0700

    create player as triangle

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
     pygame.init()
-    clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
-    print("Starting asteroids!")
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+
+        # update game state
+        player.update(dt)  # Move things, check collisions
+
+        # render
         screen.fill("black")
+        player.draw(screen) # Draw everything in its new position
         pygame.display.flip()
-        
+
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
+
 if __name__ == "__main__":
     main()
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
